a0ce639f8d761fbe56ff7a50428737af6dca74f8
stew anoya <stewartanoya@gmail.com>
2022-01-04 00:18:45 +0000

    use sum calculation

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_rev = Store.sum("annual_revenue")
+
+puts total_rev
+
+average_rev = total_rev / Store.count()
+puts average_rev
+
+money_makers = Store.where("annual_revenue >= 1000000").count()
+puts money_makers

fd00bb7a9dc5595e53d140052b33c35f00096e6a
stew anoya <stewartanoya@gmail.com>
2022-01-04 00:50:03 +0000

    create employees using instance variables as FK

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Stew", last_name: "Anoya", hourly_rate: 90)
+@store1.employees.create(first_name: "Francis", last_name: "Bourgoin", hourly_rate: 100)
+@store2.employees.create(first_name: "James", last_name: "Bain", hourly_rate: 90)
+@store2.employees.create(first_name: "Vasily", last_name: "Klimkin", hourly_rate: 200)
+@store2.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 250)

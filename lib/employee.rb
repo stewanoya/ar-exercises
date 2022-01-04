7061d83e53af37d2b8ab47d7a7356c5da5212a15
stew anoya <stewartanoya@gmail.com>
2022-01-04 00:50:26 +0000

    add required validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_rate,  numericality: { greater_than_or_equal_to: 40 }
+  validates :hourly_rate,  numericality: { less_than_or_equal_to: 200 }
 
+
+  belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
 
+  has_many :employees
 end
